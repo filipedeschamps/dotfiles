99be5a970d6031caf4db5ce765a0747dd9aba8b5
Filipe Deschamps <filiped@gmail.com>
2016-02-13 09:13:25 -0200

    feat(git): add .gitconfig and diff-so-fany

--- install.sh
+++ install.sh
@@ -4,5 +4,11 @@ echo "Symlinking VIM"
 
 ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.vim ~/.vim
+ln -s $(pwd)/.gitconfig ~/.gitconfig
+
+# BINARIES
+echo "Installing binaries"
+brew install gnu-sed
+npm install -g diff-so-fancy
 
 echo "Done!"
