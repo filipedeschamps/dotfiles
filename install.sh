3a97fc4e4fa8c524efe08d8ac58b7885ecdeba8d
Filipe Deschamps <filiped@gmail.com>
2016-09-04 11:34:24 -0300

    feat(tmux): add config files

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.vim ~/.vim
 ln -s $(pwd)/.gitconfig ~/.gitconfig
 ln -s $(pwd)/.zshrc ~/.zshrc
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 
 # BINARIES
 echo "Installing binaries:"
